5dea2a8c4fbed8ae62b8d5380fd343f3bf1ac3c6
ahmotameni <a.h.motameni@gmail.com>
2020-08-28 15:17:20 +0430

    add simple login

--- NoName/NoName/urls.py
+++ NoName/NoName/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('NoNameApp/', include('NoNameApp.urls')),
+    path('', include('NoNameApp.urls')),
 ]
